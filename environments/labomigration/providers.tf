864010ee47c7fee39efd420733bef4cc75f3ac0e
DEadMan10sds <alejandroadan.sds@gmail.com>
2026-03-31 17:50:30 -0600

    ADD: SSH connection to providers

--- environments/labomigration/providers.tf
+++ environments/labomigration/providers.tf
@@ -12,5 +12,11 @@ terraform {
 provider "proxmox" {
   endpoint  = var.proxmox_endpoint
   api_token = var.proxmox_api_token
-  insecure  = true  # mientras no tengas cert válido en Proxmox
+  insecure  = true
+
+  ssh {
+    agent       = false
+    username    = "root"
+    private_key = file("~/.ssh/id_ed25519")
+  }
 }
