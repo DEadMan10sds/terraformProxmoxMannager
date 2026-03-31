d107529e03a04d1a0ee7fc54f0639a11d67b559c
DEadMan10sds <alejandroadan.sds@gmail.com>
2026-03-31 16:52:29 -0600

    INITIAL TERRAFORM STRUCTURE

---
+++ environments/labomigration/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.73"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = var.proxmox_endpoint
+  api_token = var.proxmox_api_token
+  insecure  = true  # mientras no tengas cert válido en Proxmox
+}
---
+++ environments/labomigration/variables.tf
@@ -0,0 +1,16 @@
+variable "proxmox_endpoint" {
+  description = "URL de la API de Proxmox"
+  type        = string
+}
+
+variable "proxmox_api_token" {
+  description = "API token de Proxmox en formato user@realm!tokenid=secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "proxmox_node" {
+  description = "Nodo de Proxmox donde se crearán los recursos"
+  type        = string
+  default     = "server1"
+}
