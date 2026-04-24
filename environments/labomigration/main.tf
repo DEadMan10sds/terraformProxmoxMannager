c35b94c130a9266f0d317f57444c1a5c2c78f5da
DEadMan10sds <alejandroadan.sds@gmail.com>
2026-04-24 10:14:44 -0600

    ADD: Disk size sync

--- environments/labomigration/main.tf
+++ environments/labomigration/main.tf
@@ -139,7 +139,7 @@ module "Piggybank" {
   cores          = 4
   sockets        = 2
   memory         = 8192
-  disk_size      = 64
+  disk_size      = 80
   datastore_id   = "VMStorage"
   disk_interface = "scsi0"
   boot_order     = ["scsi0"]
